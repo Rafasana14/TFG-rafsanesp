c28a7d7ea92677b47371580f5a47c5201aee7339
Rafael Sanabria Espárrago <rafasana9@gmail.com>
2023-06-04 23:34:33 +0200

    Estilo de imágenes home

--- frontend/src/home/index.js
+++ frontend/src/home/index.js
@@ -17,9 +17,9 @@ class Home extends Component {
     async componentDidMount() {
         const dog = await (await fetch("https://some-random-api.com/animal/dog")).json();
         this.setState({ dog: dog, });
-        const cat = await (await fetch("https://some-random-api.com/animal/cat")).json();
+        const cat = await (await fetch("https://some-random-api.com/animal/bird")).json();
         this.setState({ cat: cat, });
-        const panda = await (await fetch("https://some-random-api.com/animal/red_panda")).json();
+        const panda = await (await fetch("https://some-random-api.com/animal/kangaroo")).json();
         this.setState({ panda: panda, });
     }
 
@@ -33,7 +33,7 @@ class Home extends Component {
                     <Row>
                         <Col>
                             <div>
-                                <img style={{ maxHeight: "300px" }} src={dog.image} alt='random dog' />
+                                <img className="home-img" src={dog.image} alt='random dog' />
                                 <p>{dog.fact}</p>
                             </div>
                         </Col>
@@ -45,13 +45,13 @@ class Home extends Component {
                     <Row>
                         <Col>
                             <div>
-                                <img style={{ maxHeight: "300px" }} src={cat.image} alt='random cat' />
+                                <img className="home-img" src={cat.image} alt='random cat' />
                                 <p>{cat.fact}</p>
                             </div>
                         </Col>
                         <Col>
                             <div>
-                                <img style={{ maxHeight: "300px" }} src={panda.image} alt='random panda' />
+                                <img className="home-img" src={panda.image} alt='random panda' />
                                 <p>{panda.fact}</p>
                             </div>
                         </Col>
